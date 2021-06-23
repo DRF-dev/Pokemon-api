3e9388c545c8444ecf91cac0e9788e3164cf4c3c
DRF-dev <dany.francisco@outlook.com>
2021-06-23 11:16:30 +0200

    chore(init): first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokemon API',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Aucun pokemon pour le moment'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/accueil.view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AccueilView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
